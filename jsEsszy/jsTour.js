5f094db740c05c6cbd5277eb36a67b8231ee653c
vkvaibhavtb <vaibhavkumar_rajkumar@srmuniv.edu.in>
2023-07-10 22:56:27 +0530

    jsTxtObjArr

---
+++ jsEsszy/jsTour.js
@@ -0,0 +1,20 @@
+console.log("tourguide")
+//declare a varibale named x 
+let x;
+//value assignment of x
+x = 0;
+//js supports several types of varibales
+x = 1;
+x = 0.01;
+x = "hello world";
+//boolean varibale 
+x = true ; 
+let booksx = {topic : "JAvascript ", edition : 7 };
+console.log(booksx);
+console.log("book name ",booksx.topic);
+booksx.author = "Vaibhhav raj kumar ";
+console.log(booksx);
+booksx.price = "$888";
+console.log("Book price is ",booksx.price)
+console.log("books data is ",booksx);
+
---
+++ jsEsszy/jsTourArrayObject.js
@@ -0,0 +1,29 @@
+let primes = [2,3,4,5,6,7,8];
+
+console.log(primes.length);
+
+console.log(primes[primes.length-1]);
+
+
+console.log(primes[2]);
+
+let c = "#"+45; //acts as a concatenated string
+
+console.log(c);
+
+let points = [
+    {x:0,y:12},
+    {x:23,y:45}
+];
+
+console.log(points);
+
+let data = {
+    trial1 : [[1,2],[3,4]],
+    trial2 : [[7,16],[16,25]]
+};
+
+console.log(data);
+console.log("Trials 1 ",data.trial1);
+console.log("Trials 2 ",data.trial2);
+
